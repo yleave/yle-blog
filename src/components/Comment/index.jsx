509dbf55ce452b390fffe3db3f2de89373268212
wei.ye <yleavesw@163.com>
2021-04-22 16:05:43 +0800

    fix:修复 build 时出现的 window is not defined 问题

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -2,16 +2,37 @@ import React from 'react';
 import GitalkComponent from "gitalk/dist/gitalk-component";
 import md5 from 'md5';
 
-export default function() {
-    return (
-        <GitalkComponent options={{
-            clientID: "376f48b355e1f9ac5d2e",
-            clientSecret: "aceda79297e0265d1ae12cae6216e367f6ad7a3e", 
-            repo: "gittalk-comments",
-            owner: "yleave",
-            admin: ["yleave"],
-            id: md5(location.pathname)
-        }} />
-    );
-};
+export default class Comment extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            ready: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            ready: true
+        });
+    }
+
+    render() {
+        return (
+            <>
+                {
+                    this.state.ready ? 
+                        <GitalkComponent options={{
+                            clientID: "376f48b355e1f9ac5d2e",
+                            clientSecret: "aceda79297e0265d1ae12cae6216e367f6ad7a3e", 
+                            repo: "gittalk-comments",
+                            owner: "yleave",
+                            admin: ["yleave"],
+                            id: md5(location.pathname)
+                        }} />
+                        : null
+                }
+            </>
+        );
+    }
+}
  
